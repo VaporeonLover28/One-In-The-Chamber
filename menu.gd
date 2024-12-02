491c197de528e1aea26823a57bca99f4a9466028
VaporeonLover28 <waltercarvalho2904@gmail.com>
2024-12-02 14:50:55 -0300

    menu anim + fadeout
    - instructions fade in
    - insructions

--- menu.gd
+++ menu.gd
@@ -1,8 +1,17 @@
 extends Node2D
 
 func _ready() -> void:
-	await get_tree().create_timer(0.5).timeout
+	await get_tree().create_timer(1.5).timeout
 	$anim.play("menu")
 
 func _on_plei_button_up() -> void:
-	pass
+	$black.z_index = 10
+	$anim.play("change")
+	await get_tree().create_timer(0.5).timeout
+	get_tree().change_scene_to_file("res://instructions.tscn")
+
+func _on_opissoes_button_up() -> void:
+	pass # Replace with function body.
+
+func _on_cuite_button_up() -> void:
+	get_tree().quit()
